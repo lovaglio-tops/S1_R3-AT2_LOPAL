911feb3125f8fa564975e8f451b5c08b0722cac9
unknown <lovagliodaniel@gmail.com>
2025-04-08 16:18:10 -0300

    criado um programa para saber se o usuario é maior de idade t

--- atv.js
+++ atv.js
@@ -1,8 +1,8 @@
-const idade = parseInt(prompt("Digite sua idade:"));
+const idade = parseInt(prompt("Digite sua idade:"));//constante para saber a idade do usuario
 
-if(isNaN(idade)|| idade<0){
+if(isNaN(idade)|| idade<0){// para saber se o usuario digitou algo invalido
     alert("Você digitou um caracter errado:");
-}else{
+}else{//para saber se o usuario é maior de idade
     if(idade>=18){
         alert("Voçê é maior de idade:")
     }else{
