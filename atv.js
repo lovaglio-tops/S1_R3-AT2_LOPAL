2cd43dba3f3794bdbc82cc1da7ad304b4c798a79
unknown <lovagliodaniel@gmail.com>
2025-04-08 16:06:11 -0300

    coloquei comentarios no ultimo commit

--- atv.js
+++ atv.js
@@ -1,1 +1,14 @@
-const numero = parseFloat(prompt("Digite um numero"))
+const numero = parseFloat(prompt("Digite um numero"));// contante para armazenar o numero do usuario
+
+if(isNaN(numero)){//caso o usuario nao digite um numero ele vai apontar um erro
+    alert("erro você não digitou um numero:");
+}else{//se nao ele vai analisar se é positivo ou negativo
+    if(numero>0){
+        alert("numero é positivo");   
+    }else{
+        alert("numero é negativo")
+    }
+} 
+   
+    
+    
