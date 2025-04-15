e54c6db61f04d3f2173f1977720ae19ff358a362
unknown <lovagliodaniel@gmail.com>
2025-04-15 08:40:04 -0300

    criado um programa para descobrir se numero é multiplo de

--- atv.js
+++ atv.js
@@ -1,10 +1,8 @@
-const valorCompra = parseInt(prompt("Digite o valor da sua compra:"));
-const desconto = valorCompra*0.1
-const compraDesconto = valorCompra-desconto
-if(isNaN(valorCompra)|| valorCompra<100||valorCompra<0){
-    alert("Voçe digitou um valor de compra que não tem desconto")
-}else{
-    if(valorCompra>=100){
-    alert("sua compra com desconto é:"+compraDesconto);
-    }
+const numero = parseInt(prompt("Digite um numero:"));//constante para saber o numero do usuario
+if(isNaN(numero)|| numero<0){//para saber se oque usuario digitou é valido
+    alert("numero ou caracter invalido");
+}else if(numero%5==0){//para saber se numero é multiplo de 5 
+    alert("seu numero é multiplo de cinco");
+} else{
+    alert("numero não é multiplo de 5");
 }
