19a47b1df9a4296151d86d5a171898c1b3e6af8e
unknown <lovagliodaniel@gmail.com>
2025-04-15 08:47:28 -0300

    colocado comentario em um programa para saber o valor de uma compra com desconto de 10%

--- atv.js
+++ atv.js
@@ -1,9 +1,9 @@
-const valorCompra = parseInt(prompt("Digite o valor da sua compra:"));
-const desconto = valorCompra*0.1
-const compraDesconto = valorCompra-desconto
-if(isNaN(valorCompra)|| valorCompra<100||valorCompra<0){
+const valorCompra = parseInt(prompt("Digite o valor da sua compra:"));//saber o numero do usuario
+const desconto = valorCompra*0.1// valor do desconto 
+const compraDesconto = valorCompra-desconto// valor da compra com desconte de 10%
+if(isNaN(valorCompra)|| valorCompra<100||valorCompra<0){ // para saber se a compra tem desconto
     alert("Voçe digitou um valor de compra que não tem desconto")
-}else{
+}else{//saber o valor da compra com desconto
     if(valorCompra>=100){
     alert("sua compra com desconto é:"+compraDesconto);
     }
