73a5aad388994e67d28711bccd87cbc883144fd2
unknown <lovagliodaniel@gmail.com>
2025-04-15 08:44:43 -0300

    foi colocado comentario em um programa para descobrir se o numero é par ou impar

--- atv.js
+++ atv.js
@@ -1,7 +1,7 @@
-const numPar = parseInt(prompt("Digite um numero"));
-if(isNaN(numPar)|| numPar<0){
+const numPar = parseInt(prompt("Digite um numero"));// contante para saber o numero do usuario 
+if(isNaN(numPar)|| numPar<0){// para saber se oque os usuario digitou é valido
     alert("erro seu numero não é valido");
-}else{
+}else{//para saber se o numero é par 
     if(numPar%2==0){
         alert("seu numero é par:");
     }else{
