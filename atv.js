ca1c3322f31402388b8873a930199cf000f8f572
unknown <lovagliodaniel@gmail.com>
2025-04-15 08:21:33 -0300

    criado um programa para saber o valor de uma compra com desconto de 10%

--- atv.js
+++ atv.js
@@ -1,12 +1,10 @@
-const numPar = parseInt(prompt("Digite um numero"));
-if(isNaN(numPar)|| numPar<0){
-    alert("erro seu numero não é valido");
+const valorCompra = parseInt(prompt("Digite o valor da sua compra:"));
+const desconto = valorCompra*0.1
+const compraDesconto = valorCompra-desconto
+if(isNaN(valorCompra)|| valorCompra<100||valorCompra<0){
+    alert("Voçe digitou um valor de compra que não tem desconto")
 }else{
-    if(numPar%2==0){
-        alert("seu numero é par:");
-    }else{
-        alert("seu numero é impar:");
+    if(valorCompra>=100){
+    alert("sua compra com desconto é:"+compraDesconto);
     }
 }
-
-    
